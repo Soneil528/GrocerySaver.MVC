3323071e1db786880d28ac0a26cc254ee3dcafb8
Soneil528 <73606666+Soneil528@users.noreply.github.com>
2021-07-22 19:20:33 -0400

    beverage controller, list and index added

---
+++ GrocerySaver.WebMVC/Controllers/BeverageController.cs
@@ -0,0 +1,20 @@
+﻿using GrocerySaver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrocerySaver.WebMVC.Controllers
+{
+    [Authorize]
+    public class BeverageController : Controller
+    {
+        // GET: Beverage
+        public ActionResult Index()
+        {
+            var model = new BeverageListItem[0];
+            return View(model);
+        }
+    }
+}

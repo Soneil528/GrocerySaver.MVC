5d8be6b0265b60fcd2576438fd3a9f32525b2b30
Soneil528 <73606666+Soneil528@users.noreply.github.com>
2021-07-22 19:30:48 -0400

    dairy controller, list and index added

---
+++ GrocerySaver.Models/DairyListItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrocerySaver.Models
+{
+    public class DairyListItem
+    {
+        [Display(Name = "Id")]
+        public int DairyId { get; set; }
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Display(Name = "Days Left to Expire")]
+        public int ShelfLifeInDays { get; set; }
+        [Display(Name = "Quantity")]
+        public int Count { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
---
+++ GrocerySaver.WebMVC/Controllers/DairyController.cs
@@ -0,0 +1,20 @@
+﻿using GrocerySaver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrocerySaver.WebMVC.Controllers
+{
+    [Authorize]
+    public class DairyController : Controller
+    {
+        // GET: Dairy
+        public ActionResult Index()
+        {
+            var model = new DairyListItem[0];
+            return View(model);
+        }
+    }
+}

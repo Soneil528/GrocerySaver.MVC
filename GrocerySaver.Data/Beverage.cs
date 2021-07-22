d6ce996cd5cfa59ebac7bb34bbcc83c27e0c4c7d
Soneil Pitamber <pitamber@att.net>
2021-07-22 15:20:58 -0400

    small edits forgot to commit

--- GrocerySaver.Data/Beverage.cs
+++ GrocerySaver.Data/Beverage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,17 @@ namespace GrocerySaver.Data
 {
     public class Beverage
     {
+        [Key]
         public int BeverageId { get; set; }
+        [Required]
         public Guid OwnerId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int ShelfLifeInDays { get; set; }
+        [Required]
         public int AmountInOunces { get; set; }
+        [Required]
         public int Count { get; set; }
 
         public DateTimeOffset CreatedUtc { get; set; }
--- GrocerySaver.Data/Dairy.cs
+++ GrocerySaver.Data/Dairy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace GrocerySaver.Data
 {
     public class Dairy
     {
+        [Key]
+        public int DairyId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int ShelfLifeInDays { get; set; }
+        [Required]
+        public int AmountInOunces { get; set; }
+        [Required]
+        public int Count { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }

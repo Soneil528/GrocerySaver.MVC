a43e39e9319e12306a776cf9980f21ae2e1400d5
Soneil528 <73606666+Soneil528@users.noreply.github.com>
2021-07-22 19:06:41 -0400

    meat controller, list and index view added

---
+++ GrocerySaver.Models/MeatListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrocerySaver.Models
+{
+    public class MeatListItem //Index view
+    {
+        [Display(Name = "Id")]
+        public int MeatId { get; set; }
+        [Display(Name="Type")]
+        public string Name { get; set; }
+        [Display(Name = "Days Left to Expire")]
+        public int ShelfLifeInDays { get; set; }
+        [Display(Name = "Weight in Ounces")]
+        public int AmountInOunces { get; set; }
+        [Display(Name = "Quantity")]
+        public int Count { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+    }
+}
---
+++ GrocerySaver.WebMVC/Controllers/MeatController.cs
@@ -0,0 +1,20 @@
+﻿using GrocerySaver.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrocerySaver.WebMVC.Controllers
+{
+    [Authorize]
+    public class MeatController : Controller
+    {
+        // GET: Meat
+        public ActionResult Index()
+        {
+            var model = new MeatListItem[0];
+            return View(model);
+        }
+    }
+}
